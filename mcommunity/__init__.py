5d7534d671b3e3527af1d012e9b1e150b67c3761
Maggie Davidson <jmagdavidson@gmail.com>
2023-02-07 11:57:17 -0500

    Add classes to __all__

---
+++ mcommunity/__init__.py
@@ -0,0 +1,5 @@
+from mcommunity.mcommunity_base import MCommunityBase
+from mcommunity.mcommunity_group import MCommunityGroup
+from mcommunity.mcommunity_user import MCommunityUser
+
+__all__ = [MCommunityBase, MCommunityGroup, MCommunityUser]
